a327354602bbbbbf5f274c04b304cc467f3e4c43
Haru <jonggun02@gmail.com>
2025-03-07 13:34:01 +0900

    계층수정

--- Ch.py
+++
@@ -1,2 +0,0 @@
-def Pretest():
-    return 0
---
+++ Pyprog/Ch1.py
@@ -0,0 +1,6 @@
+""" from collections import Counter
+f=open("input.txt",encoding='utf-8')
+count=Counter(f.read().split())
+print(count)
+f.close() """
+#이런게 있따! 
